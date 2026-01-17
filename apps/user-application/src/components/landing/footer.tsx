13f5e832d0743e7f67f92ca965a78bb9af57dc44
codemathic <56730371arzivre@users.noreply.github.com>
2026-01-17 21:42:51 +0700

    chore: fix and clean up footer

--- apps/user-application/src/components/landing/footer.tsx
+++ apps/user-application/src/components/landing/footer.tsx
@@ -1,23 +1,10 @@
 import { Link } from "@tanstack/react-router";
 import { ExternalLink } from "lucide-react";
-import { FaXTwitter, FaGithub } from "react-icons/fa6";
 
 const navigation = {
   main: [
     { name: "Privacy Policy", href: "/privacy-policy" },
     { name: "Terms of Service", href: "/tos" },
-  ],
-  social: [
-    {
-      name: "GitHub",
-      href: "https://github.com/backpine/saas-kit",
-      icon: FaGithub,
-    },
-    {
-      name: "X",
-      href: "https://x.com/backpinelabs",
-      icon: FaXTwitter,
-    },
   ],
 };
 
@@ -28,7 +15,7 @@ export function Footer() {
         <div className="flex flex-col md:flex-row md:items-center md:space-x-8 space-y-6 md:space-y-0">
           <div>
             <h3 className="text-sm font-semibold text-foreground">
-              TanStack Ecosystem
+              Resource
             </h3>
             <ul role="list" className="mt-2 space-y-1">
               {navigation.main.map((item) => (
@@ -45,27 +32,8 @@ export function Footer() {
             </ul>
           </div>
         </div>
-
         <div className="mt-8 md:mt-0 flex flex-col md:flex-row md:items-center space-y-4 md:space-y-0 md:space-x-6">
-          {/* <div className="flex space-x-6">
-            {navigation.social.map((item) => {
-              const IconComponent = item.icon;
-              return (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-muted-foreground hover:text-foreground transition-colors"
-                >
-                  <span className="sr-only">{item.name}</span>
-                  <IconComponent className="h-5 w-5" />
-                </a>
-              );
-            })}
-          </div> */}
-
-          <div className="text-center md:text-right">
+          <div className="">
             <p className="text-xs text-muted-foreground">
               This is an fan site. Assets & trademarks belong to their owners.
             </p>
